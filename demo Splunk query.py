39c4709809c19bface535f6c6f37fc6fa4956e3f
admin <root@localhost>
2022-02-17 19:56:32 +0000

    demo update notable

--- demo Splunk query.py
+++ demo Splunk query.py
@@ -143,7 +143,82 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
         "create_list:formatted_data"
     ]
 
-    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters)
+    # responses
+    response_types = [
+        {
+            "prompt": "Would you like to own the notable and change status to open",
+            "options": {
+                "type": "list",
+                "choices": [
+                    "Yes",
+                    "No"
+                ],
+            },
+        },
+        {
+            "prompt": "Please enter a comment for the notable",
+            "options": {
+                "type": "message",
+            },
+        }
+    ]
+
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters, response_types=response_types, callback=decision_1)
+
+    return
+
+
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_1() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["prompt_1:action_result.summary.responses.0", "==", "Yes"]
+        ])
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        update_event_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+def update_event_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("update_event_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    prompt_1_result_data = phantom.collect2(container=container, datapath=["prompt_1:action_result.summary.responses.1","prompt_1:action_result.parameter.context.artifact_id"], action_results=results)
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.event_id","artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'update_event_1' call
+    for prompt_1_result_item in prompt_1_result_data:
+        for container_artifact_item in container_artifact_data:
+            if container_artifact_item[0] is not None:
+                parameters.append({
+                    "owner": "analyst",
+                    "status": "in progress",
+                    "comment": prompt_1_result_item[0],
+                    "event_ids": container_artifact_item[0],
+                    "context": {'artifact_id': container_artifact_item[1]},
+                })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("update event", parameters=parameters, name="update_event_1", assets=["splunk100"])
 
     return
 
