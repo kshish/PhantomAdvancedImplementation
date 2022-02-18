24a319208d835902a0549555a87cd1dfcac35076
admin <root@localhost>
2022-02-18 18:01:49 +0000

    Februarty cf demo

---
+++ custom_functions/someDemo.py
@@ -0,0 +1,25 @@
+def someDemo(myIp=None, myHostname=None, myString=None, **kwargs):
+    """
+    Good description goes here
+    
+    Args:
+        myIp (CEF type: ip): Enter or map an IP address
+        myHostname (CEF type: hostname): map a hostname value
+        myString: map any value
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        myoutIP (CEF type: ip): Some IP output from cf
+        someValue: any data type
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    
+    outputs= {"myoutIP" : myIp, "someValue" : "Chris wuz here"}
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
