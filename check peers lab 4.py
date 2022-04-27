0859cae4a94190ab6ae169972586b835dd49b2a7
admin <root@localhost>
2022-04-27 15:17:07 +0000

    lab 5 demo triggering create containers from list

--- check peers lab 4.py
+++ check peers lab 4.py
@@ -266,13 +266,21 @@ def l5_cf_get_query_results_py3_soar53_3(action=None, success=None, container=No
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="PAI/L5_CF_Get_Query_Results_py3_SOAR53", parameters=parameters, name="l5_cf_get_query_results_py3_soar53_3", callback=playbook_1)
+    phantom.custom_function(custom_function="PAI/L5_CF_Get_Query_Results_py3_SOAR53", parameters=parameters, name="l5_cf_get_query_results_py3_soar53_3", callback=playbook_create_containers_from_list_soar53_1)
 
     return
 
 
-def playbook_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("playbook_1() called")
+def playbook_create_containers_from_list_soar53_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("playbook_create_containers_from_list_soar53_1() called")
+
+    l5_cf_get_query_results_py3_soar53_3__result = phantom.collect2(container=container, datapath=["l5_cf_get_query_results_py3_soar53_3:custom_function_result.data.results_list_name"])
+
+    l5_cf_get_query_results_py3_soar53_3_data_results_list_name = [item[0] for item in l5_cf_get_query_results_py3_soar53_3__result]
+
+    inputs = {
+        "list_name": l5_cf_get_query_results_py3_soar53_3_data_results_list_name,
+    }
 
     ################################################################################
     ## Custom Code Start
@@ -284,8 +292,8 @@ def playbook_1(action=None, success=None, container=None, results=None, handle=N
     ## Custom Code End
     ################################################################################
 
-    # call playbook "local/playbook", returns the playbook_run_id
-    playbook_run_id = phantom.playbook("local/playbook", container=container)
+    # call playbook "PAI/Create Containers from list SOAR53", returns the playbook_run_id
+    playbook_run_id = phantom.playbook("PAI/Create Containers from list SOAR53", container=container, inputs=inputs)
 
     return
 
