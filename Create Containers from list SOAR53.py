91c110c1635057c20df91b73df930a1badb35ac1
admin <root@localhost>
2022-04-28 17:54:29 +0000

    debug

--- Create Containers from list SOAR53.py
+++ Create Containers from list SOAR53.py
@@ -39,7 +39,18 @@ def l5_cf_retrieve_list_soar53_2(action=None, success=None, container=None, resu
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="PAI/L5_CF_Retrieve_List_SOAR53", parameters=parameters, name="l5_cf_retrieve_list_soar53_2", callback=l5_cf_create_containers_from_list_py3_soar53_3)
+    phantom.custom_function(custom_function="PAI/L5_CF_Retrieve_List_SOAR53", parameters=parameters, name="l5_cf_retrieve_list_soar53_2", callback=l5_cf_retrieve_list_soar53_2_callback)
+
+    return
+
+
+def l5_cf_retrieve_list_soar53_2_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("l5_cf_retrieve_list_soar53_2_callback() called")
+
+    
+    l5_cf_create_containers_from_list_py3_soar53_3(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=filtered_artifacts, filtered_results=filtered_results)
+    debug_4(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=filtered_artifacts, filtered_results=filtered_results)
+
 
     return
 
@@ -69,6 +80,43 @@ def l5_cf_create_containers_from_list_py3_soar53_3(action=None, success=None, co
     ################################################################################
 
     phantom.custom_function(custom_function="PAI/L5_CF_Create_Containers_From_List_py3_SOAR53", parameters=parameters, name="l5_cf_create_containers_from_list_py3_soar53_3")
+
+    return
+
+
+def debug_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("debug_4() called")
+
+    l5_cf_retrieve_list_soar53_2__result = phantom.collect2(container=container, datapath=["l5_cf_retrieve_list_soar53_2:custom_function_result.data.listContents"])
+
+    l5_cf_retrieve_list_soar53_2_data_listcontents = [item[0] for item in l5_cf_retrieve_list_soar53_2__result]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": l5_cf_retrieve_list_soar53_2_data_listcontents,
+        "input_2": None,
+        "input_3": None,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_4")
 
     return
 
