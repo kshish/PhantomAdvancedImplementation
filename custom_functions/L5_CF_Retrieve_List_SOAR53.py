c14252a73509d83c91f569972e33f073774fa400
admin <root@localhost>
2022-04-28 17:24:22 +0000

    Retrieve list

---
+++ custom_functions/L5_CF_Retrieve_List_SOAR53.py
@@ -0,0 +1,24 @@
+def L5_CF_Retrieve_List_SOAR53(listName=None, **kwargs):
+    """
+    This CF retrieves the contents of a list
+    
+    Args:
+        listName
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        listContents
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    
+    read_list__peer_list = phantom.get_list(listName)
+    outputs = {"listContents": read_list__peer_list}
+    
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
